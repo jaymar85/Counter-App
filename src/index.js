117881cc59b5325ca351d910c11971f663e3bfd3
jaymar85 <jermark851@gmail.com>
2020-04-28 18:04:34 -0500

    complete render classes dynamically

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Counter from './components/counter';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
